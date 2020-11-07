f059e70786f186438d435d2e223025093830e622
Sonu Ram <sonuram6960@gmail.com>
2020-11-07 21:35:09 +0530

    Update pyramid solution

--- problems/pyramid/index.js
+++ problems/pyramid/index.js
@@ -14,34 +14,14 @@
 //       ' ### '
 //       '#####'
 
-// Recursion
-
-function pyramid(n, row = 0, level = "") {
-  if (row === n) return;
-
-  if (level.length === n + n - 1) {
-    console.log(level);
-    return pyramid(n, row + 1);
-  }
-
+// Main Solution
+function pyramid(n) {
   const mid = Math.floor((n + n - 1) / 2);
 
-  if (level.length >= mid - row && level.length <= mid + row) {
-    level += "#";
-  } else {
-    level += " ";
-  }
-  pyramid(n, row, level);
-}
-
-module.exports = pyramid;
-
-/* function pyramid(n) {
-  const mid = Math.floor((n + n - 1) / 2);
   for (let row = 0; row < n; row++) {
-    let level = "",
-      start = mid - row,
-      end = mid + row;
+    let level = "";
+    const start = mid - row;
+    const end = mid + row;
 
     for (let col = 0; col < n + n - 1; col++) {
       if (col >= start && col <= end) {
@@ -53,4 +33,28 @@ module.exports = pyramid;
 
     console.log(level);
   }
-} */
+}
+
+// Alternate Solutions
+function pyramidWithRecursion(n, row = 0, level = "") {
+  if (row === n) return;
+
+  if (level.length === n + n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const mid = Math.floor((n + n - 1) / 2);
+  const start = mid - row;
+  const end = mid + row;
+
+  if (level.length >= start && level.length <= end) {
+    level += "#";
+  } else {
+    level += " ";
+  }
+
+  pyramid(n, row, level);
+}
+
+module.exports = pyramid;
