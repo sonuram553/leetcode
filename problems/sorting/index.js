f4c2877699b6e0f4ffa408629c6c186744c92671
Sonu Ram <sonuram6960@gmail.com>
2020-12-25 21:56:46 +0530

    Remove alternate selection-sort logic

--- problems/sorting/index.js
+++ problems/sorting/index.js
@@ -34,16 +34,6 @@ function bubbleSort(arr) {
 } */
 
 function selectionSort(arr) {
-  /* for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] <= arr[j]) continue;
-      const temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
-    }
-  } */
-
-  // Alternate solution
   for (let i = 0; i < arr.length - 1; i++) {
     let indexOfMin = i;
     for (let j = i + 1; j < arr.length; j++) {
