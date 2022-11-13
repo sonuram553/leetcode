304a4deb6ae82f9f6547aa82ff442ce556a0cede
Sonu Ram <sonuram6960@gmail.com>
2022-11-13 20:00:48 +0530

    Max Depth of Binary Tree

--- easy/maximum-depth-of-binary-tree.js
+++ easy/maximum-depth-of-binary-tree.js
@@ -1,15 +1,8 @@
-const maxDepth = function (root) {
-  if (!root) return 0;
+function maxDepth(root) {
+  if (root === null) return 0;
 
-  const stack = [root];
-  let count = 1;
+  const left = maxDepth(root.left);
+  const right = maxDepth(root.right);
 
-  while (stack.length) {
-    const node = stack.pop();
-    if (node.left || node.right) count += 1;
-    if (node.left) stack.push(node.left);
-    if (node.right) stack.push(node.right);
-  }
-
-  return count;
-};
+  return Math.max(left, right) + 1;
+}
