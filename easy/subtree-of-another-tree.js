ee0b75c643ed38bff5d5dfa85f26f36b8699931f
Sonu Ram <sonuram6960@gmail.com>
2022-11-15 23:20:44 +0530

    Subtree of Another Tree

---
+++ easy/subtree-of-another-tree.js
@@ -0,0 +1,29 @@
+function isSubtree(root, subRoot) {
+  let isFoundSubtree = false;
+
+  function preOrder(node) {
+    if (!node) return;
+
+    if (equalTrees(node, subRoot)) isFoundSubtree = true;
+
+    if (!isFoundSubtree) {
+      preOrder(node.left);
+      preOrder(node.right);
+    }
+  }
+
+  preOrder(root);
+  return isFoundSubtree;
+}
+
+function equalTrees(firstTreeNode, secondTreeNode) {
+  if (firstTreeNode === null && secondTreeNode === null) return true;
+  if (firstTreeNode === null || secondTreeNode === null) return false;
+
+  if (firstTreeNode.val !== secondTreeNode.val) return false;
+
+  return (
+    equalTrees(firstTreeNode.left, secondTreeNode.left) &&
+    equalTrees(firstTreeNode.right, secondTreeNode.right)
+  );
+}
